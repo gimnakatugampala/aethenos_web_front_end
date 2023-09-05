1e861924569c74ad4720602bab0d28a4ed27da79
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-09-05 10:34:43 +0530

    commit

--- src/commonFunctions/Colors.js
+++ src/commonFunctions/Colors.js
@@ -1,3 +1,10 @@
 export default {
-    
+    PrimaryColor:'#EC8736',
+    SecondaryColor:'#EF542F',
+    ButtonColors:{'background-color':'#EF542F','border-color':'#EF542F','color':'#fff'},
+    IconColors:{'color':'#EF542F'},
+    TextColor:{'color':'#EC8736','text-decoration':'none'},
+    RecieveTextMessageBubble:{'background-color':'#EC8736','border-color':'#EC8736','color':'#fff'},
+    SendTextMessageBubble:{'background-color':'#EF542F','border-color':'#EF542F','color':'#fff'},
+    BadgePill:{'background-color':'#EF542F','color':'#fff','padding':'2px 10px 2px 10px','border-radius':'10px','z-index':'1','font-size':'10px'}
 }
