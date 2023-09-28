157a005bc1034b377a4afc4c66b140307972c69e
Ashen Indula <123031352+ashen9917@users.noreply.github.com>
2023-09-28 21:54:17 +0530

    Review_box_Ashen_indula_2023.09.28

--- src/pages/reviews/Reviews.js
+++ src/pages/reviews/Reviews.js
@@ -6,47 +6,68 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
-import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
 import ModeCommentTwoToneIcon from '@mui/icons-material/ModeCommentTwoTone';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import ReviewCommentBox from './ReviewCommentBox';
+import Rating from '@mui/material/Rating';
 
-const GridContainer = styled('div')({
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)', // Three columns in each row
-  gridGap: '16px', // Adjust as needed
-  maxWidth: '900px', // Adjust the total width as needed
-  margin: '0 auto', // Center the grid
+const CardContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'stretch',
+  gap: '16px',
+  width: '100%', // Cards take the full width of the parent container
+  maxWidth: '100%',
 });
 
-interface ExpandMoreProps extends IconButtonProps {
-  onClick: () => void; // Add an onClick prop
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  return <IconButton {...props} />;
-})();
+const cardData = [
+  {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    rating: 5,
+    description: 'Web developer with a passion for clean code.',
+  },
+  {
+    name: 'Alice Smith',
+    email: 'alicesmith@gmail.com',
+    rating: 4,
+    description: 'Front-end developer and UI/UX enthusiast.',
+  },
+  {
+    name: 'Bob Johnson',
+    email: 'bobjohnson@gmail.com',
+    rating: 3,
+    description: 'Full-stack developer with experience in Node.js.',
+  },
+  {
+    name: 'Emma Wilson',
+    email: 'emmawilson@gmail.com',
+    rating: 5,
+    description: 'Tech blogger and JavaScript enthusiast.',
+  },
+];
 
 export default function RecipeReviewCard() {
-  const [commentBoxes, setCommentBoxes] = React.useState(Array(6).fill(false));
+  const [expanded, setExpanded] = React.useState(Array(4).fill(false));
 
-  const toggleCommentBox = (index) => {
-    const newCommentBoxes = [...commentBoxes];
-    newCommentBoxes[index] = !newCommentBoxes[index];
-    setCommentBoxes(newCommentBoxes);
+  const handleExpandClick = (index) => {
+    const newExpanded = [...expanded];
+    newExpanded[index] = !newExpanded[index];
+    setExpanded(newExpanded);
   };
 
   return (
-    <GridContainer>
-      {Array.from({ length: 6 }).map((_, index) => (
-        <Card key={index} sx={{ maxWidth: 300 }}>
+    <CardContainer>
+      {cardData.map((card, index) => (
+        <Card key={index}>
           <CardHeader
             avatar={
               <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                D
+                {card.name.charAt(0)}
               </Avatar>
             }
             action={
@@ -54,30 +75,39 @@ export default function RecipeReviewCard() {
                 <MoreVertIcon />
               </IconButton>
             }
-            title="Doro Onome Churchill"
-            subheader="nomzykush@gmail.com"
+            title={card.name}
+            subheader={
+              <>
+                {card.email}
+                <br />
+                <Rating
+                  name={`rating-${index}`}
+                  value={card.rating}
+                  readOnly
+                />
+              </>
+            }
           />
           <CardContent>
             <Typography variant="body2" color="text.secondary">
-              Front-end developer and Technical writer with keen attention to detail and a passion for delivering content as interactive as it is educational.
+              {card.description}
             </Typography>
           </CardContent>
           <CardActions disableSpacing>
-            <ExpandMore
-              onClick={() => toggleCommentBox(index)}
+            <IconButton
+              onClick={() => handleExpandClick(index)}
               aria-label="show more"
             >
               <ModeCommentTwoToneIcon />
-            </ExpandMore>
+            </IconButton>
           </CardActions>
-          <Collapse in={commentBoxes[index]} timeout="auto" unmountOnExit>
+          <Collapse in={expanded[index]} timeout="auto" unmountOnExit>
             <CardContent sx={{ maxHeight: '200px', overflowY: 'auto' }}>
-              {/* Increase maxHeight and add overflowY */}
               <ReviewCommentBox />
             </CardContent>
           </Collapse>
         </Card>
       ))}
-    </GridContainer>
+    </CardContainer>
   );
 }
