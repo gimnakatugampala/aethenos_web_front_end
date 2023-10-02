64b0ffa2e4d973308e2940ef4b912f5f5a41ce67
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-10-02 19:17:17 +0530

    2023_10_02_Curriculum_2

--- src/pages/courses/manage-courses/curriculum/Curriculum.js
+++ src/pages/courses/manage-courses/curriculum/Curriculum.js
@@ -15,58 +15,10 @@ const { Header, Footer, Sider, Content } = Layout;
 const { Text, Link , Title } = Typography;
 
 
-const fileList = [
-  // {
-  //   uid: '0',
-  //   name: 'xxx.png',
-  //   status: 'uploading',
-  //   percent: 33,
-  // },
-  {
-    uid: '-1',
-    name: 'yyy.png',
-    status: 'done',
-    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  },
-  {
-    uid: '-2',
-    name: 'yyy.png',
-    status: 'done',
-    url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  },
-  // {
-  //   uid: '-2',
-  //   name: 'zzz.png',
-  //   status: 'error',
-  // },
-];
-
-  
 
  const Curriculum = () => {
 
-  const [show, setShow] = useState(false);
-  const [showCurriculum, setshowCurriculum] = useState(false)
-  const [showLecture, setshowLecture] = useState(false)
-   
-    
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const handleCurriculum = () => setshowCurriculum(true)
-    const handleCurriculumClose = () => setshowCurriculum(false)
-
-    const handleshowLecture = () => setshowLecture(!showLecture)
-    const handleshowLectureClose = () => setshowLecture(false)
-
-
-    const onDrop = (data) => {
-      console.log(data)
-      // => banana 
-  }
 
   return (
     <div className='col-md-8'>
