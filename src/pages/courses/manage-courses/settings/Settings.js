8ac4ad21f50d538d5f16122aff1cbc054f2cb0d6
Ashen Indula <123031352+ashen9917@users.noreply.github.com>
2023-10-04 14:37:04 +0530

    Setting Page_Ashen_Indula_2023.10.04

--- src/pages/courses/manage-courses/settings/Settings.js
+++ src/pages/courses/manage-courses/settings/Settings.js
@@ -1,20 +1,46 @@
-import React from 'react'
-import './Settings.css'
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from 'react';
+import { Typography, Button, Paper, Container, Box, Grid } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import PublishIcon from '@mui/icons-material/Publish';
 
-import { Card  } from 'antd';
-const Settings = () => {
+function App() {
   return (
-    <div className='col-md-8'>
-    <Card className="py-2 my-2"> 
-        <Typography className='p-3' variant='h4'>
-          Seetings
-       </Typography>
-       <hr />
-       </Card>
-       </div>
-  )
+    <Container maxWidth="md">
+      <Box my={4}>
+        <Paper elevation={3} sx={{ padding: 3, borderRadius: 2 }}>
+          <Typography variant="h4" align="center" gutterBottom>
+            Settings
+          </Typography>
+          <Typography variant="h6" color="error" align="center" gutterBottom>
+            DISAPPROVED
+          </Typography>
+          <Typography variant="body1" align="center" paragraph>
+            This content has been disapproved.
+          </Typography>
+          <Grid container spacing={2} justifyContent="center">
+            <Grid item>
+              <Button
+                variant="contained"
+                color="secondary"
+                startIcon={<DeleteIcon />}
+              >
+                Delete
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<PublishIcon />}
+              >
+                Unpublish
+              </Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
+    </Container>
+  );
 }
 
-export default Settings
+export default App;
