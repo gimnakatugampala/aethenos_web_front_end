1fbfd279b24097c85f281d171a7d3e2b734c2b37
Ashen Indula <123031352+ashen9917@users.noreply.github.com>
2023-10-05 02:12:18 +0530

    Setting_page_Ashen_Indula_2023.10.05

--- src/pages/courses/manage-courses/settings/Settings.js
+++ src/pages/courses/manage-courses/settings/Settings.js
@@ -1,40 +1,53 @@
 import React from 'react';
-import { Typography, Button, Paper, Container, Box, Grid } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
-import PublishIcon from '@mui/icons-material/Publish';
+import { Typography, Paper, Container, Box, Button, Grid } from '@mui/material';
 
 function App() {
   return (
     <Container maxWidth="md">
       <Box my={4}>
-        <Paper elevation={3} sx={{ padding: 3, borderRadius: 2 }}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Settings
-          </Typography>
-          <Typography variant="h6" color="error" align="center" gutterBottom>
+        <Paper elevation={0} sx={{ padding: 3, borderRadius: 2, backgroundColor: '#f0f0f0', borderColor: 'red' }}>
+          <Typography variant="h6" color="error" gutterBottom>
             DISAPPROVED
           </Typography>
-          <Typography variant="body1" align="center" paragraph>
+          <Typography variant="body1" gutterBottom>
             This content has been disapproved.
           </Typography>
-          <Grid container spacing={2} justifyContent="center">
-            <Grid item>
-              <Button
-                variant="contained"
-                color="secondary"
-                startIcon={<DeleteIcon />}
-              >
-                Delete
-              </Button>
+        </Paper>
+      </Box>
+      <Box my={4}>
+        <Paper elevation={3} sx={{ padding: 3, borderRadius: 2 }}>
+          <Typography variant="h6" gutterBottom>
+            Do you want to publish this Course?
+          </Typography>
+          <Typography variant="body1">
+            By unpublishing, your students won't be able to buy your course.
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid container justifyContent="flex-end">
+                <Button variant="contained" color="secondary">
+                  Unpublish
+                </Button>
+              </Grid>
             </Grid>
-            <Grid item>
-              <Button
-                variant="contained"
-                color="primary"
-                startIcon={<PublishIcon />}
-              >
-                Unpublish
-              </Button>
+          </Grid>
+        </Paper>
+      </Box>
+      <Box my={4}>
+        <Paper elevation={3} sx={{ padding: 3, borderRadius: 2 }}>
+          <Typography variant="h6" gutterBottom>
+            Do you want to Delete this Course?
+          </Typography>
+          <Typography variant="body1">
+            By deleting, your course will be invisible to everyone, even you.
+          </Typography>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Grid container justifyContent="flex-end">
+                <Button variant="contained" color="primary">
+                  Delete
+                </Button>
+              </Grid>
             </Grid>
           </Grid>
         </Paper>
