586396057c702a34a8685fab153445ad5b7d1fad
Thilan Wijesinghe <thilan.ruwanga665@gmail.com>
2023-10-05 20:12:02 +0530

    2023_10_05_Curriculum

--- src/pages/courses/manage-courses/curriculum/Curriculum.js
+++ src/pages/courses/manage-courses/curriculum/Curriculum.js
@@ -1,172 +1,112 @@
-import React, { useState } from 'react';
-import { Container, Button, TextField, Box, TextareaAutosize, Divider } from '@mui/material';
+import React, { useState } from "react";
+import { Card, Typography, Button } from "@mui/material";
+import "./curriculum.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const App = () => {
-  const [sectionTitle, setSectionTitle] = useState('');
-  const [learningObjectives, setLearningObjectives] = useState('');
+const Curriculum = () => {
+    const [showSection, setShowSection] = useState(false);
 
-  const [lectureTitle, setLectureTitle] = useState('');
-  const [quizTitle, setQuizTitle] = useState('');
-  const [quizContent, setQuizContent] = useState('');
-  const [exerciseTitle, setExerciseTitle] = useState('');
-  const [assignmentTitle, setAssignmentTitle] = useState('');
+    const toggleSection = () => {
+        setShowSection(!showSection);
+    };
 
-  const handleAddSection = () => {
-    // Logic to handle adding section
-  };
-
-  const handleAddLecture = () => {
-    // Logic to handle adding lecture
-  };
-
-  const handleAddQuiz = () => {
-    // Logic to handle adding quiz
-  };
-
-  const handleAddExercise = () => {
-    // Logic to handle adding coding exercise
-  };
-
-  const handleAddAssignment = () => {
-    // Logic to handle adding assignment
-  };
-
-  return (
-    <Container maxWidth="md" mt={5}>
-      <Box mb={4}>
-        <Button variant="contained" color="primary">+Section</Button>
-        <div>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Enter title"
-            value={sectionTitle}
-            onChange={(e) => setSectionTitle(e.target.value)}
-          />
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Enter learning objectives"
-            value={learningObjectives}
-            onChange={(e) => setLearningObjectives(e.target.value)}
-            mt={2}
-          />
-          <Button variant="outlined" color="secondary" mt={2}>Cancel</Button>
-          <Button variant="contained" color="success" mt={2} onClick={handleAddSection}>
-            Add Section
-          </Button>
-        </div>
-      </Box>
-
-      <div>
-        <Button variant="contained" color="primary">Curriculum item</Button>
-      </div>
-
-      <div>
-        <Button variant="contained" color="info" style={{ marginTop: '10px' }}>Lecture</Button>
-        <div>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Lecture Title"
-            value={lectureTitle}
-            onChange={(e) => setLectureTitle(e.target.value)}
-          />
-          <div>
-            <Button variant="contained" color="success" onClick={handleAddLecture}>
-              Add Lecture
-            </Button>
-            <Button variant="outlined" color="secondary" mt={2}>Cancel</Button>
-            <br /> {/* Line break */}
-            <Button variant="contained" color="primary" mt={2}>
-              Content
-            </Button>
-          </div>
-        </div>
-        <Divider />
-      </div>
-
-      <div>
-        <Button variant="contained" color="info" style={{ marginTop: '10px' }}>Quiz</Button>
-        <div>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Quiz Title"
-            value={quizTitle}
-            onChange={(e) => setQuizTitle(e.target.value)}
-          />
-          <TextareaAutosize
-            placeholder="Quiz Content"
-            value={quizContent}
-            onChange={(e) => setQuizContent(e.target.value)}
-            mt={2}
-            rowsMin={3}
-            fullWidth
-          />
-          <div>
-            <Button variant="contained" color="success" onClick={handleAddQuiz}>
-              Add Quiz
-            </Button>
-            <Button variant="outlined" color="secondary" mt={2}>Cancel</Button>
-            <br /> {/* Line break */}
-            <Button variant="contained" color="primary" mt={2}>
-              Content
-            </Button>
-          </div>
-        </div>
-        <Divider />
-      </div>
-
-      <div>
-        <Button variant="contained" color="info" style={{ marginTop: '10px' }}>Coding Exercises</Button>
-        <div>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Exercises Title"
-            value={exerciseTitle}
-            onChange={(e) => setExerciseTitle(e.target.value)}
-          />
-          <div>
-            <Button variant="contained" color="success" onClick={handleAddExercise}>
-              Add Exercise
-            </Button>
-            <Button variant="outlined" color="secondary" mt={2}>Cancel</Button>
-            <br /> {/* Line break */}
-            <Button variant="contained" color="primary" mt={2}>
-              Content
-            </Button>
-          </div>
-        </div>
-        <Divider />
-      </div>
-
-      <div>
-        <Button variant="contained" color="info" style={{ marginTop: '10px' }}>Assignment</Button>
-        <div>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Assignment Title"
-            value={assignmentTitle}
-            onChange={(e) => setAssignmentTitle(e.target.value)}
-          />
-          <div>
-            <Button variant="contained" color="success" onClick={handleAddAssignment}>
-              Add Assignment
-            </Button>
-            <Button variant="outlined" color="secondary" mt={2}>Cancel</Button>
-            <br /> {/* Line break */}
-            <Button variant="contained" color="primary" mt={2}>
-              Content
-            </Button>
-          </div>
-        </div>
-        <Divider />
-      </div>
-    </Container>
-  );
+    return ( <
+        div className = "col-md-8" >
+        <
+        Card className = "py-2 my-2" >
+        <
+        Typography className = "p-3"
+        variant = "h4"
+        style = {
+            { display: "flex", justifyContent: "space-between" } } >
+        <
+        span > Curriculum < /span> <Button> Bulk Uploader </Button > { " " } <
+        /Typography>{" "} <
+        hr / >
+        <
+        div className = "row" >
+        <
+        h6 className = "mb-3" >
+        Start putting together your course by creating sections, lectures,
+        and practice(quizzes, coding exercises, and assignments).Start putting together your course by creating sections, lectures, and practice activities(quizzes, coding exercises, and assignments).Use your course outline to structure your content and label your sections and lectures clearly.If you 're intending to offer your
+        course
+        for free, the total length of video content must be less than 2 hours. { " " } <
+        /h6>{" "} <
+        Button className = "col-2 mb-3"
+        onClick = { toggleSection } > { " " } +
+        { " " } <
+        /Button>{" "} {
+            showSection && ( <
+                div className = "bgc mb-3 flex-column" >
+                <
+                div className = "d-flex" >
+                <
+                span className = "fw-bold"
+                style = {
+                    { marginRight: "10px" } } > { " " }
+                New Section: { " " } <
+                /span>{" "} <
+                textarea className = "col-11 tex1"
+                rows = "" / >
+                <
+                /div>{" "} <
+                div >
+                <
+                span className = "fw-bold" > { " " }
+                What will students be able to do at the end of this section ? { " " } <
+                    /span>{" "} <
+                    textarea className = "col-10 tex1"
+                rows = "" / >
+                <
+                /div>{" "} <
+                div className = "text-end" >
+                <
+                Button > Cancel < /Button> <Button> Add Section </Button > { " " } <
+                /div>{" "} <
+                /div>
+            )
+        } { " " } <
+        div className = "bgc mb-3" >
+        <
+        span className = "fw-bold" > { " " }
+        Section 1: < span > Introduction < /span>{" "} <
+        /span>{" "} <
+        div className = "d-flex justify-content-between align-items-center bgc mb-3 mt-3" >
+        <
+        span > { " " }
+        Lecture 1: < span > Introduction < /span>{" "} <
+        /span>{" "} <
+        Button > +Content < /Button>{" "} <
+        /div>{" "} <
+        Button > Curriculum Item < /Button>{" "} <
+        /div>{" "} <
+        Button className = "col-2 mt-3" > +Section < /Button>{" "} {
+            /* {showSection && (
+                        <div className="bgc mb-3 flex-column">
+                          <div className="d-flex">
+                            <span className="fw-bold" style={{ marginRight: "10px" }}>
+                              {" "}
+                              New Section:{" "}
+                            </span>{" "}
+                            <textarea className="col-11 tex1" rows="" />
+                          </div>{" "}
+                          <div>
+                            <span className="fw-bold">
+                              {" "}
+                              What will students be able to do at the end of this section ?{" "}
+                            </span>{" "}
+                            <textarea className="col-10 tex1" rows="" />
+                          </div>{" "}
+                          <div className="text-end">
+                            <Button> Cancel </Button> <Button> Add Section </Button>{" "}
+                          </div>{" "}
+                        </div>
+                      )}{" "} */
+        } <
+        /div>{" "} <
+        /Card>{" "} <
+        /div>
+    );
 };
 
-export default App;
+export default Curriculum;
