8bd4c0def3805fb49de9a4a520c8d7cc65d53b8a
Ashen Indula <123031352+ashen9917@users.noreply.github.com>
2023-10-09 03:23:32 +0530

    Q&A_Ashen_Indula_2023.10.09

--- src/pages/communications/qa/QA.js
+++ src/pages/communications/qa/QA.js
@@ -1,9 +1,114 @@
-import React from 'react'
-import './QA.css'
+import React from "react";
+import ScrollBarPage from "./ScrollBarPage";
+import "./QA.css";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Questions from "./Questions";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import Card from "react-bootstrap/Card";
+
 const QA = () => {
   return (
-    <div>QA</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-1">
+          <p className="fs-5 font-bold">Q&A</p>
+        </div>
+        <div className="col-2">
+          <select className="form-select w-70">
+            <option value="all_courses">All Courses</option>
+            <option value="course_1">Course 1</option>
+            <option value="course_2">Course 2</option>
+            <option value="course_3">Course 3</option>
+            <option value="course_4">Course 4</option>
+          </select>
+        </div>
+      </div>
+      <div className="container"></div>
+      <div className="row">
+        <div className="col-2">
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox defaultChecked />}
+              label="Unread"
+            />
+          </FormGroup>
+        </div>
+        <div className="col-2">
+          <FormGroup>
+            <FormControlLabel
+              Notopanswer
+              control={<Checkbox />}
+              label="No top answer"
+            />
+          </FormGroup>
+        </div>
+        <div className="col-2">
+          <FormGroup>
+            <FormControlLabel
+              noAnswer
+              control={<Checkbox />}
+              label="No answers"
+            />
+          </FormGroup>
+        </div>
+        <div className="col-3">
+          <FormGroup>
+            <FormControlLabel
+              noIinstructorAnswer
+              control={<Checkbox />}
+              label="No instructor answer"
+            />
+          </FormGroup>
+        </div>
+        <div className="col-3">
+          <div className="row">
+            <div className="col-3">
+              <p>Sort by:</p>
+            </div>
+            <div className="col-9">
+              <select className="form-select w-70">
+                <option value="newestToOldest">Newest to Oldest</option>
+                <option value="oldestToNewest">Oldest to Newest</option>
+                <option value="priceLowToHigh">Price: Low to High</option>
+                <option value="priceHighToLow">Price: High to Low</option>
+                <option value="ratingHighToLow">Rating: High to Low</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
 
-export default QA
+      <Card>
+        <div className="row">
+          <div className="col-3">
+            <Questions />
+          </div>
+          <div className="col-9">
+            <ScrollBarPage />
+            <InputGroup className="mb-3">
+              <Form.Control
+                placeholder="Post a public answer"
+                aria-label="Post a public answer"
+                aria-describedby="basic-addon2"
+              />
+              <div className="col-2">
+                <Button
+                  className="w-90"
+                  variant="outline-secondary bg-primary text-light"
+                  id="button-addon2"
+                >
+                  Publish
+                </Button>
+              </div>
+            </InputGroup>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+};
+export default QA;
---
+++ src/pages/communications/qa/ScrollBarPage.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { MDBContainer } from "mdbreact";
+import "./QA.css";
+
+const ScrollBarPage = () => {
+  const scrollContainerStyle = { width: "800px", maxHeight: "500px" };
+  return (
+    <MDBContainer>
+      <div
+        className="scrollbar scrollbar-primary  mt-5 mx-auto"
+        style={scrollContainerStyle}
+      >
+        <p>
+          [Your Name] [Your Address] [City, State, ZIP Code] [Email Address]
+          [Date] [Recipient's Name] [Recipient's Address] [City, State, ZIP
+          Code] Subject: A Letter of Appreciation and Gratitude Dear
+          [Recipient's Name], I hope this letter finds you in the best of
+          spirits and health. I am writing to express my deepest appreciation
+          and gratitude for the countless ways in which you have positively
+          impacted my life. In this fast-paced world where genuine kindness
+          sometimes feels like a rare commodity, your actions have shone
+          brightly as beacons of goodness and humanity. I want to take this
+          opportunity to acknowledge the warmth and generosity you have
+          consistently shown, not only to me but to everyone fortunate enough to
+          know you. Your ability to empathize with others, your willingness to
+          lend a helping hand, and your contagious positivity have made you a
+          source of inspiration for me and many others. One of the qualities I
+          admire most about you is your unwavering kindness. Your capacity to
+          understand the struggles of others and offer support, without
+          judgment, is truly remarkable. Whether it was a word of encouragement
+          during challenging times or a simple act of kindness that brightened
+          my day, your gestures have left a lasting impact on my heart and soul.
+          Your generosity knows no bounds. I have witnessed your selfless acts,
+          from volunteering at local shelters to organizing fundraisers for
+          noble causes. Your dedication to making the world a better place has
+          inspired me to become a more compassionate and socially conscious
+          individual. Your actions have shown me the importance of giving back
+          to the community and helping those in need. I am also deeply grateful
+          for the countless moments of joy and laughter you have shared with me.
+          Your sense of humor and ability to find happiness in the simplest of
+          things have added colors to my life. I cherish the memories we have
+          created together and look forward to many more moments of shared
+          laughter and camaraderie in the future. In addition to your kindness
+          and generosity, I am profoundly thankful for your wisdom and guidance.
+          Your insights have been invaluable to me, helping me navigate life's
+          challenges with grace and resilience. Your advice has provided me with
+          a different perspective, allowing me to grow as an individual and
+          learn from my experiences. I want you to know that your positive
+          influence has not only touched my life but has also inspired those
+          around you. Your actions have created a ripple effect, encouraging
+          others to be kinder, more compassionate, and empathetic. The world
+          needs more people like you, individuals who lead by example and make a
+          difference in the lives of others. As I conclude this letter, I want
+          to reiterate my heartfelt gratitude for everything you have done for
+          me and for others. Your kindness, generosity, and wisdom have made a
+          significant impact on my life, and I am genuinely thankful to have you
+          as a part of my journey. Please remember that your efforts, no matter
+          how small they may seem to you, have the power to change the world.
+          Your positivity and compassion are contagious, and I am confident that
+          you will continue to inspire and uplift those around you. Once again,
+          thank you from the bottom of my heart for being the wonderful person
+          that you are. I am blessed to know you, and I am looking forward to
+          many more years of friendship and shared experiences. Wishing you all
+          the happiness, health, and success in the world. With warmest regards,
+          [Your Name] Feel free to customize the letter further to better suit
+          your needs or add specific details about the recipient.
+        </p>
+      </div>
+    </MDBContainer>
+  );
+};
+export default ScrollBarPage;
