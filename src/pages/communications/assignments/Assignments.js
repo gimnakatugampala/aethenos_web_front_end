64f3a348b92cc68119f68dd1ebe444dbedd287b4
Ashen Indula <123031352+ashen9917@users.noreply.github.com>
2023-10-09 21:24:01 +0530

    Assignment_Update_Ashen_Indula_2023.10.09

--- src/pages/communications/assignments/Assignments.js
+++ src/pages/communications/assignments/Assignments.js
@@ -9,6 +9,7 @@ import Instruction from "./Instructions";
 import SubmissionForm from "./SubmissionForm";
 import Instructor from "./Instructor";
 import Feedback from "./Feedback";
+import Card from "react-bootstrap/Card";
 import Inputs from "./Inputs";
 
 const steps = [
@@ -64,60 +65,62 @@ export default function HorizontalLinearStepper() {
   };
 
   return (
-    <Box sx={{ width: "100%" }}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-          }
-          // if (isStepSkipped(index)) {
-          // stepProps.completed = false;
-          // }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Box sx={{ flex: "1 1 auto" }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          {activeStep === 0 && <Instruction />}
-          {activeStep === 1 && <SubmissionForm />}
-          {activeStep === 2 && <Instructor />}
-          {activeStep === 3 && <Feedback />}
-          <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: "1 1 auto" }} />
-            {/* {isStepOptional(activeStep) && (
+    <Card>
+      <Box sx={{ width: "100%" }}>
+        <Stepper activeStep={activeStep}>
+          {steps.map((label, index) => {
+            const stepProps = {};
+            const labelProps = {};
+            if (isStepOptional(index)) {
+            }
+            // if (isStepSkipped(index)) {
+            // stepProps.completed = false;
+            // }
+            return (
+              <Step key={label} {...stepProps}>
+                <StepLabel {...labelProps}>{label}</StepLabel>
+              </Step>
+            );
+          })}
+        </Stepper>
+        {activeStep === steps.length ? (
+          <React.Fragment>
+            <Typography sx={{ mt: 2, mb: 1 }}>
+              All steps completed - you&apos;re finished
+            </Typography>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Box sx={{ flex: "1 1 auto" }} />
+              <Button onClick={handleReset}>Reset</Button>
+            </Box>
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <div className="p-2">{activeStep === 0 && <Instruction />}</div>
+            <div className="p-2">{activeStep === 1 && <SubmissionForm />}</div>
+            <div className="p-2">{activeStep === 2 && <Instructor />}</div>
+            <div className="p-2">{activeStep === 3 && <Feedback />}</div>
+            <Box sx={{ display: "flex", flexDirection: "row", pt: 2 }}>
+              <Button
+                color="inherit"
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                sx={{ mr: 1 }}
+              >
+                Back
+              </Button>
+              <Box sx={{ flex: "1 1 auto" }} />
+              {/* {isStepOptional(activeStep) && (
               <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
                 Skip
               </Button>
             )} */}
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? "Finish" : "Next"}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
-    </Box>
+              <Button onClick={handleNext}>
+                {activeStep === steps.length - 1 ? "Finish" : "Next"}
+              </Button>
+            </Box>
+          </React.Fragment>
+        )}
+      </Box>
+    </Card>
   );
 }
--- src/pages/communications/assignments/Inputs.js
+++ src/pages/communications/assignments/Inputs.js
@@ -1,279 +1,121 @@
-import { InboxOutlined, UploadOutlined } from "@ant-design/icons";
 import React from "react";
-import {
-  Button,
-  Checkbox,
-  Col,
-  Form,
-  InputNumber,
-  Radio,
-  Rate,
-  Row,
-  Select,
-  Slider,
-  Space,
-  Switch,
-  Upload,
-} from "antd";
-import ColorPicker from "./ColorPicker";
-
-const { Option } = Select;
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
 const formItemLayout = {
   labelCol: {
-    span: 6,
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 4,
+    },
   },
   wrapperCol: {
-    span: 14,
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 20,
+    },
   },
 };
-const normFile = (e) => {
-  console.log("Upload event:", e);
-  if (Array.isArray(e)) {
-    return e;
-  }
-  return e?.fileList;
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 20,
+      offset: 4,
+    },
+  },
 };
-const onFinish = (values) => {
-  console.log("Received values of form: ", values);
-};
-const App = () => (
-  <Form
-    name="validate_other"
-    {...formItemLayout}
-    onFinish={onFinish}
-    initialValues={{
-      "input-number": 3,
-      "checkbox-group": ["A", "B"],
-      rate: 3.5,
-      "color-picker": null,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-  >
-    <Form.Item label="Plain Text">
-      <span className="ant-form-text">China</span>
-    </Form.Item>
-    <Form.Item
-      name="select"
-      label="Select"
-      hasFeedback
-      rules={[
-        {
-          required: true,
-          message: "Please select your country!",
-        },
-      ]}
-    >
-      <Select placeholder="Please select a country">
-        <Option value="china">China</Option>
-        <Option value="usa">U.S.A</Option>
-      </Select>
-    </Form.Item>
-
-    <Form.Item
-      name="select-multiple"
-      label="Select[multiple]"
-      rules={[
-        {
-          required: true,
-          message: "Please select your favourite colors!",
-          type: "array",
-        },
-      ]}
-    >
-      <Select mode="multiple" placeholder="Please select favourite colors">
-        <Option value="red">Red</Option>
-        <Option value="green">Green</Option>
-        <Option value="blue">Blue</Option>
-      </Select>
-    </Form.Item>
-
-    <Form.Item label="InputNumber">
-      <Form.Item name="input-number" noStyle>
-        <InputNumber min={1} max={10} />
-      </Form.Item>
-      <span
-        className="ant-form-text"
-        style={{
-          marginLeft: 8,
-        }}
-      >
-        machines
-      </span>
-    </Form.Item>
-
-    <Form.Item name="switch" label="Switch" valuePropName="checked">
-      <Switch />
-    </Form.Item>
-
-    <Form.Item name="slider" label="Slider">
-      <Slider
-        marks={{
-          0: "A",
-          20: "B",
-          40: "C",
-          60: "D",
-          80: "E",
-          100: "F",
-        }}
-      />
-    </Form.Item>
-
-    <Form.Item name="radio-group" label="Radio.Group">
-      <Radio.Group>
-        <Radio value="a">item 1</Radio>
-        <Radio value="b">item 2</Radio>
-        <Radio value="c">item 3</Radio>
-      </Radio.Group>
-    </Form.Item>
-
-    <Form.Item
-      name="radio-button"
-      label="Radio.Button"
-      rules={[
-        {
-          required: true,
-          message: "Please pick an item!",
-        },
-      ]}
-    >
-      <Radio.Group>
-        <Radio.Button value="a">item 1</Radio.Button>
-        <Radio.Button value="b">item 2</Radio.Button>
-        <Radio.Button value="c">item 3</Radio.Button>
-      </Radio.Group>
-    </Form.Item>
-
-    <Form.Item name="checkbox-group" label="Checkbox.Group">
-      <Checkbox.Group>
-        <Row>
-          <Col span={8}>
-            <Checkbox
-              value="A"
-              style={{
-                lineHeight: "32px",
-              }}
-            >
-              A
-            </Checkbox>
-          </Col>
-          <Col span={8}>
-            <Checkbox
-              value="B"
-              style={{
-                lineHeight: "32px",
-              }}
-              disabled
-            >
-              B
-            </Checkbox>
-          </Col>
-          <Col span={8}>
-            <Checkbox
-              value="C"
-              style={{
-                lineHeight: "32px",
-              }}
-            >
-              C
-            </Checkbox>
-          </Col>
-          <Col span={8}>
-            <Checkbox
-              value="D"
-              style={{
-                lineHeight: "32px",
-              }}
-            >
-              D
-            </Checkbox>
-          </Col>
-          <Col span={8}>
-            <Checkbox
-              value="E"
-              style={{
-                lineHeight: "32px",
-              }}
-            >
-              E
-            </Checkbox>
-          </Col>
-          <Col span={8}>
-            <Checkbox
-              value="F"
-              style={{
-                lineHeight: "32px",
-              }}
-            >
-              F
-            </Checkbox>
-          </Col>
-        </Row>
-      </Checkbox.Group>
-    </Form.Item>
-
-    <Form.Item name="rate" label="Rate">
-      <Rate />
-    </Form.Item>
-
-    <Form.Item
-      name="upload"
-      label="Upload"
-      valuePropName="fileList"
-      getValueFromEvent={normFile}
-      extra="longgggggggggggggggggggggggggggggggggg"
-    >
-      <Upload name="logo" action="/upload.do" listType="picture">
-        <Button icon={<UploadOutlined />}>Click to upload</Button>
-      </Upload>
-    </Form.Item>
-    <Form.Item label="Dragger">
-      <Form.Item
-        name="dragger"
-        valuePropName="fileList"
-        getValueFromEvent={normFile}
-        noStyle
-      >
-        <Upload.Dragger name="files" action="/upload.do">
-          <p className="ant-upload-drag-icon">
-            <InboxOutlined />
-          </p>
-          <p className="ant-upload-text">
-            Click or drag file to this area to upload
-          </p>
-          <p className="ant-upload-hint">
-            Support for a single or bulk upload.
-          </p>
-        </Upload.Dragger>
-      </Form.Item>
-    </Form.Item>
-    <Form.Item
-      name="color-picker"
-      label="ColorPicker"
-      rules={[
-        {
-          required: true,
-          message: "color is required!",
-        },
-      ]}
-    >
-      <ColorPicker />
-    </Form.Item>
-
-    <Form.Item
-      wrapperCol={{
-        span: 12,
-        offset: 6,
+const App = () => {
+  const onFinish = (values) => {
+    console.log("Received values of form:", values);
+  };
+  return (
+    <Form
+      name="dynamic_form_item"
+      {...formItemLayoutWithOutLabel}
+      onFinish={onFinish}
+      style={{
+        maxWidth: 600,
       }}
     >
-      <Space>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-        <Button htmlType="reset">reset</Button>
-      </Space>
-    </Form.Item>
-  </Form>
-);
+      <Form.List
+        name="names"
+        rules={[
+          {
+            validator: async (_, names) => {
+              if (!names || names.length < 2) {
+                return Promise.reject(new Error("At least 2 Answers"));
+              }
+            },
+          },
+        ]}
+      >
+        {(fields, { add, remove }, { errors }) => (
+          <>
+            {fields.map((field, index) => (
+              <Form.Item
+                {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+                label={index === 0 ? "Answers" : ""}
+                required={false}
+                key={field.key}
+              >
+                <Form.Item
+                  {...field}
+                  validateTrigger={["onChange", "onBlur"]}
+                  rules={[
+                    {
+                      required: true,
+                      whitespace: true,
+                      message: "Please input Answers or delete this field.",
+                    },
+                  ]}
+                  noStyle
+                >
+                  <Input
+                    placeholder="Answer"
+                    style={{
+                      width: "60%",
+                    }}
+                  />
+                </Form.Item>
+                {fields.length > 1 ? (
+                  <MinusCircleOutlined
+                    className="dynamic-delete-button"
+                    onClick={() => remove(field.name)}
+                  />
+                ) : null}
+              </Form.Item>
+            ))}
+            <Form.Item>
+              <Button
+                type="dashed"
+                onClick={() => add()}
+                style={{
+                  width: "60%",
+                }}
+                icon={<PlusOutlined />}
+              >
+                Add field
+              </Button>
+              <Form.ErrorList errors={errors} />
+            </Form.Item>
+          </>
+        )}
+      </Form.List>
+      <Form.Item>
+        <Button
+          variant="contained"
+          color="primary"
+          placeholder="Add Your Answer"
+        ></Button>
+      </Form.Item>
+    </Form>
+  );
+};
 export default App;
