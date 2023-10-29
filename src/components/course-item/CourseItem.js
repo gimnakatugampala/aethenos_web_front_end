28ae49c777e512a426653302a95faac0443260d9
gimnakatugampala <gimnakatugampala1@gmail.com>
2023-10-29 19:29:55 +0530

    2023_10_29_Admin_Comment

--- src/components/course-item/CourseItem.js
+++ src/components/course-item/CourseItem.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import { Input, Space , Badge , Image ,Col, Divider, Row } from 'antd';
 import { Card  } from 'antd';
 import { Button, Progress } from 'antd';
-import { Avatar, List, Skeleton, Switch } from 'antd';
+import { Avatar, List, Skeleton, Switch , Popover  } from 'antd';
 import { CheckOutlined } from '@ant-design/icons';
 import Chip from '@mui/material/Chip';
-import Popover from '@mui/material/Popover';
+// import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import EditIcon from '@mui/icons-material/Edit';
@@ -26,6 +26,13 @@ const CourseItem = ({title, status, filledPercent, seenBy , adminreview}) => {
   };
 
   const open = Boolean(anchorEl);
+
+  const content = (
+    <div>
+      <h6><b>Admin Comment : </b></h6>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+    </div>
+  );
 
 
   setTimeout(() => {
@@ -56,6 +63,7 @@ const CourseItem = ({title, status, filledPercent, seenBy , adminreview}) => {
           }
         />
     </div>
+    
       
     <div className="col-md-10 p-0">
         <h5 className="card-title"><b>{title}</b> {adminreview == 'rejected' && (<a className='mx-2' href="edit-course"><EditIcon /></a>)} </h5> 
@@ -66,13 +74,15 @@ const CourseItem = ({title, status, filledPercent, seenBy , adminreview}) => {
           {adminreview == 'draft' ? (
             <Chip label="Draft" color="secondary" variant="outlined" />
           ) : adminreview == 'rejected' ? (
+            <Popover content={content} >
             <Chip 
               aria-owns={open ? 'mouse-over-popover' : undefined}
               icon={<MoreVertIcon />}
-              aria-haspopup="true"
-              onMouseEnter={handlePopoverOpen}
-              onMouseLeave={handlePopoverClose}
+              // aria-haspopup="true"
+              // onMouseEnter={handlePopoverOpen}
+              // onMouseLeave={handlePopoverClose}
               label="Rejected" color="primary" variant="outlined" />
+              </Popover>
           ) : adminreview == 'pending' && (
             <Chip label="Pending" color="success" variant="outlined" />
           ) }
@@ -86,30 +96,9 @@ const CourseItem = ({title, status, filledPercent, seenBy , adminreview}) => {
     </div>
     </div>
 
-   {/* Rejected Admin Comment */}
-      <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: 'none'
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        onClose={handlePopoverClose}
-        // disableRestoreFocus
-      >
-        <Typography  sx={{ p: 2 }}>
-        <h6><b>Admin Comment : </b></h6>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
-        </Typography>
-      </Popover>
+   
+
+
         {adminreview == 'pending' && (
             <div className='d-flex justify-content-center'> 
               <a className='card-item-link' href="/courses/manage/2023/">Manage Course</a>
