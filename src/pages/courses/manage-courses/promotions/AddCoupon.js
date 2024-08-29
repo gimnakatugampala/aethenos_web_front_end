dd113f778a047fb51b30fe26ef66194f49af8f91
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-08-29 20:29:48 +0530

    2024_eu

--- src/pages/courses/manage-courses/promotions/AddCoupon.js
+++ src/pages/courses/manage-courses/promotions/AddCoupon.js
@@ -933,7 +933,7 @@ const AddCoupon = ({code}) => {
 
     if(numberOnlyRegex.test(e.target.value)){
       setEUDiscountValue(e.target.value)
-      setEUDiscountPercent(((Number.parseFloat(e.target.value).toFixed(2) / Number.parseFloat(EUListPrice).toFixed(2)) * 100).toFixed(2))
+      setEUDiscountPercent((((Number.parseFloat(EUListPrice) - Number.parseFloat(e.target.value)) / Number.parseFloat(EUListPrice).toFixed(2)) * 100).toFixed(2))
       setEUDiscountAmount((Number.parseFloat(EUListPrice) - Number.parseFloat(e.target.value)))
     }else if(e.target.value == ""){
       setEUDiscountValue("")
