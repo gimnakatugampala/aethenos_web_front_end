3fce8a9f906fd738facd3645924ae117769f49db
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-08-29 08:37:15 +0530

    2024_romania

--- src/pages/courses/manage-courses/pricing/Pricing.js
+++ src/pages/courses/manage-courses/pricing/Pricing.js
@@ -3046,22 +3046,27 @@ const handleDefaultPercentageDiscount = (e) => {
       }
       
     // Percentage Discount Romania
-      const handleDefaultPercentageDiscountRomania = (e) =>{
-
-        if(e.target.value == ""){
-          setRomaniaDisPercent(0)
-        }
+    const handleDefaultPercentageDiscountRomania = (e) => {
+      let discountValue = parseFloat(e.target.value);
     
-        setRomaniaDisPercent(e.target.value)
-    
-        setRomaniaNetPrice((parseFloat(RomaniaListPrice) - parseFloat(RomaniaListPrice) * parseFloat(e.target.value == "" ? 0 : e.target.value)/100).toFixed(2))
-
-        console.log(e.target.value)
-
-        // Calculate Discount Amount
-        setRomaniaDisAmt((Number.parseFloat(RomaniaListPrice) - ((parseFloat(RomaniaListPrice) - parseFloat(RomaniaListPrice) * parseFloat(e.target.value == "" ? 0 : e.target.value)/100).toFixed(2))).toFixed(2))
-    
+      if (isNaN(discountValue) || discountValue < 0) {
+        discountValue = 0; // Ensure the discount is not negative or invalid
       }
+    
+      if (discountValue > 100) {
+        discountValue = 100; // Limit the discount to a maximum of 100%
+      }
+    
+      setRomaniaDisPercent(discountValue);
+    
+      setRomaniaNetPrice((parseFloat(RomaniaListPrice) - parseFloat(RomaniaListPrice) * discountValue / 100).toFixed(2));
+    
+      console.log(e.target.value);
+    
+      // Calculate Discount Amount
+      setRomaniaDisAmt((Number.parseFloat(RomaniaListPrice) - ((parseFloat(RomaniaListPrice) - parseFloat(RomaniaListPrice) * discountValue / 100).toFixed(2))).toFixed(2));
+    }
+    
 
     // -------------------
 
