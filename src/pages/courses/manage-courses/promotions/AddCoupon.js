005dc0a5577784bf1c1bd607c4260974a2dec0ec
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-08-29 20:44:54 +0530

    2024_thailand

--- src/pages/courses/manage-courses/promotions/AddCoupon.js
+++ src/pages/courses/manage-courses/promotions/AddCoupon.js
@@ -1647,7 +1647,7 @@ const AddCoupon = ({code}) => {
   
       if(numberOnlyRegex.test(e.target.value)){
         setThailandDiscountValue(e.target.value)
-        setThailandDiscountPercent(((Number.parseFloat(e.target.value).toFixed(2) / Number.parseFloat(ThailandListPrice).toFixed(2)) * 100).toFixed(2))
+        setThailandDiscountPercent((((Number.parseFloat(ThailandListPrice) - Number.parseFloat(e.target.value)) / Number.parseFloat(ThailandListPrice).toFixed(2)) * 100).toFixed(2))
         setThailandDiscountAmount((Number.parseFloat(ThailandListPrice) - Number.parseFloat(e.target.value)))
       }else if(e.target.value == ""){
         setThailandDiscountValue("")
