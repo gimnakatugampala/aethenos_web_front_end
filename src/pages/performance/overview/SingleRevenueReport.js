1292758ae5e3cef1d1a494fb48f1dd42b3752514
gimnakatugampala <gimnakatugampala1@gmail.com>
2024-11-21 18:01:47 +0530

    refunds

--- src/pages/performance/overview/SingleRevenueReport.js
+++ src/pages/performance/overview/SingleRevenueReport.js
@@ -150,7 +150,7 @@ const SingleRevenueReport = () => {
             />
           </Tab>
 
-          {/* <Tab
+          <Tab
             eventKey="refunds"
             title={
               <div className="tab-title">
@@ -190,7 +190,7 @@ const SingleRevenueReport = () => {
                 exportButton: true,
               }}
             />
-          </Tab> */}
+          </Tab>
 
         </Tabs>
       </Card>
